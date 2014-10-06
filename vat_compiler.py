321e43bd8012e6be8d34ecd7c7ca10e1a5be8f22
santoshantony <santoshantony@yahoo.com>
2014-10-06 23:05:45 +0530

    Sorted the output

--- vat_compiler.py
+++ vat_compiler.py
@@ -7,23 +7,31 @@ import os
 pd.set_option('expand_frame_repr', False)
 # setting up data
 # datatype = ['vat_no': S11,'amount': i5]
-bd = pd.read_csv('/home/santosh/Documents/b_sup_data.csv')
-hd = pd.read_csv('/home/santosh/Documents/h_sup_data.csv')
+# data = ('')
+
+# bd = pd.read_csv('/home/santosh/Documents/b_sup_data.csv')
+# hd = pd.read_csv('/home/santosh/Documents/h_sup_data.csv')
+
+
+bd = pd.read_csv('/home/santosh/Documents/aug_anx1_bo.csv')
+hd = pd.read_csv('/home/santosh/Documents/aug_anx1_ho.csv')
 
 os.system('clear')
 
 raw_input('setting up data from HeadOffice and BranchOffice...')
 
-hd.set_index('no', inplace=True)
+hd.set_index('serial_no', inplace=True)
 print hd
 print '@Head Office'
-print 'Monthly Sales in INR :',  hd['amount'].sum(), 'Vat Tax Total:', hd['tax'].sum()
+print 'Monthly Sales in INR :',  hd['Purchase_Value'].sum(), 'Vat Tax Total:', hd['VAT_CST_paid'].sum()
+print 'No of rows:', hd.count()
 # print hd.dtypes
 
-bd.set_index('no', inplace=True)
+bd.set_index('serial_no', inplace=True)
 print bd
 print '@Branch Office'
-print 'Monthly Sales in INR :',  bd['amount'].sum(), 'Vat Tax Total:', bd['tax'].sum()
+print 'Monthly Sales in INR :',  bd['Purchase_Value'].sum(), 'Vat Tax Total:', bd['VAT_CST_paid'].sum()
+print 'No of rows:', bd.count()
 
 raw_input('Above is HeadOffice and Branch Office Data.Press any key to continue....')
 # Consolidating Data
@@ -32,7 +40,13 @@ cd = hd.append(bd)
 raw_input('\nConsolidatng data.. \n')
 print cd
 print 'Consolidated figures of Head Ofice and Branch Office'
-print 'Monthly Sales in INR :',  cd['amount'].sum(), 'Vat Tax Total:', cd['tax'].sum()
+print 'Monthly Sales in INR :',  cd['Purchase_Value'].sum(), 'Vat Tax Total:', cd['VAT_CST_paid'].sum()
+print 'No of rows:', cd.count()
+# Group by vat rate
+raw_input('Grouping the data according to vat_rate')
+os.system('clear')
+gbcd = cd.sort('Tax_rate')
+print gbcd
 
 # Generating Data for upload...
 writer = pd.ExcelWriter('vat.xlsx', engine='xlsxwriter')
